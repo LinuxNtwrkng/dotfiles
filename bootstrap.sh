8a165d5d5b0c50c30385ee03bf56ad5f867fef40
Kaden <kaden@p3family.com>
2022-06-28 16:09:06 -0500

    Added Powerline font download step

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,9 @@
 # install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# install Powerline fonts
+cd ~ && git clone https://github.com/powerline/fonts.git --depth=1 && cd fonts && ./install.sh && cd ~ && rm -rf fonts
+
 # grab mackup repo
 cd ~ && git clone https://github.com/LinuxNtwrkng/mackup.git && cd ~/mackup && git remote add upstream https://github.com/lra/mackup.git && git fetch upstream && git checkout master && git rebase upstream/master
 
